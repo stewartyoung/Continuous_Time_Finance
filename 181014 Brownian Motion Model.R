ea0346bcc9cd7849baa88f979e6905b81fde10ee
stewartyoung <36263575+stewartyoung@users.noreply.github.com>
2018-11-04 19:00:55 +0000

    Add files via upload

--- 181014 Brownian Motion Model.R
+++ 181014 Brownian Motion Model.R
@@ -4,6 +4,8 @@ require(yuima)
 
 
 #setting brownian motion model
+# "init" is initial value of a stock
+# "drift" and "diffusion" coefficients as in brownian motion SDE
 bmmodel=setModel(drift="0",diffusion="1",state.variable="x",time.variable="t",
                solve.variable="x",xinit=100)
 
@@ -15,8 +17,10 @@ plot(X)
 d1=X@data@original.data
 d1
 
-#increasing n in grid makes the motion more "choppy"
-grid=setSampling(Terminal=1, n=500)
+# "Terminal" represents terminal time of process
+# "n" splits up time, such that: delta.t=Terminal/n
+# increasing n in grid makes the motion more "choppy" as there are more points to be plotted
+grid=setSampling(Terminal=1, n=10000)
 X=simulate(bmmodel,sampling=grid)
 plot(X)
 
